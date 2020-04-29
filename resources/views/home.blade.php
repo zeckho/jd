b3b175fe1d531eb9d79bc807625704790d281952
achmadjp <achmadjp7@gmail.com>
2020-04-29 14:10:23 +0700

    add bootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="mt-5">Welcome</h1>
+        </div>
+    </div>
+@endsection
