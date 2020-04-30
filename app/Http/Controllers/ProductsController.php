7bb28b28883c9a6e5e0099479c0d68b6362c6c29
achmadjp <achmadjp7@gmail.com>
2020-04-30 13:13:13 +0700

    fix

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -34,74 +34,8 @@ class ProductsController extends Controller
  
         }
  
-        // if item not exist in cart then add to cart with quantity = 1
-        // $cart[$id] = [
-        //     "name" => $product->name,
-        //     "quantity" => 1,
-        //     "price" => $product->price,
-        //     "image" => $product->image
-        // ];
-
-        // $this->saveToCart($product);
- 
         return redirect()->back()->with('success', 'Product added to cart successfully!');
     }
-    // public function addToCart($id)
-    // {
-    //     $product = Product::findOrFail($id);
-
-    //     if(!$product) { 
-    //         abort(404);
-    //     }
- 
-    //     $cart = session()->get('cart');
- 
-    //     // if cart is empty then this the first product
-    //     if(!$cart) {
- 
-    //         $cart = [
-    //             $id => [
-    //                 "name" => $product->name,
-    //                 "quantity" => 1,
-    //                 "price" => $product->price,
-    //                 "image" => $product->image
-    //             ]
-    //         ];
- 
-    //         session()->put('cart', $cart);
-
-    //         $this->saveToCart($product);
- 
-    //         return redirect()->back()->with('success', 'Product added to cart successfully!');
-    //     }
- 
-    //     // if cart not empty then check if this product exist then increment quantity
-    //     if(isset($cart[$id])) {
- 
-    //         $cart[$id]['quantity']++;
- 
-    //         session()->put('cart', $cart);
-
-    //         $this->saveToCart($product);
- 
-    //         return redirect()->back()->with('success', 'Product added to cart successfully!');
- 
-    //     }
- 
-    //     // if item not exist in cart then add to cart with quantity = 1
-    //     $cart[$id] = [
-    //         "name" => $product->name,
-    //         "quantity" => 1,
-    //         "price" => $product->price,
-    //         "image" => $product->image
-    //     ];
- 
-    //     session()->put('cart', $cart);
-
-    //     $this->saveToCart($product);
- 
-    //     return redirect()->back()->with('success', 'Product added to cart successfully!');
-    // }
 
     public function saveToCart($data)
     {
